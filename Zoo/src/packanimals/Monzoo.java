a41467528d56aedd9367df5ba59f99dbb20bf7ed
jboze <jboze@csc.com>
2014-07-28 12:13:52 +0200

    commited

--- Zoo/src/packanimals/Monzoo.java
+++ Zoo/src/packanimals/Monzoo.java
@@ -3,16 +3,17 @@ package packanimals;
 public class Monzoo {
 
 	public static void main(String[] args) {
-		Animal a = new Animal(25,3);
+		Animal a = new Chien(25,3);
 		Chien c = new Chien(25,3);
 		Lion l = new Lion(30,4);
 		
-		System.out.println(a.toString());
-		System.out.println(c.toString());
-		System.out.println(l.toString());
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(l);
 		
 		a.manger();
 		c.manger();
+		l.manger();
 	}
 
 }
